0f0b5e5e40ad1e1eec7f2e4a9c6b3022ee1f1897
izescape <izescape@icloud.com>
2022-10-31 11:48:52 +0900

    first-commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(Request $request)
+    {
+        $item= [
+            'txt' => 'テキスト',
+            'param' => $request->param
+        ];
+        return view('index', $item);
+    }
+}
